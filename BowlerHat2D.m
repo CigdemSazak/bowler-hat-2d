0ae04cf95048be812b45ad0269503f1416302812
Cigdem Sazak <cigdem.sazak@durham.ac.uk>
2018-09-07 16:12:04 +0100

    Add files via upload

---
+++ BowlerHat2D.m
@@ -0,0 +1,28 @@
+function imda = BowlerHat2D(im,si,no)
+s = 2:si; %size of the disk
+o = 0:180/no:180-180/no; %number of orientation
+
+imol = zeros(size(im,1),size(im,2),length(s),length(o));
+imod = zeros(size(im,1),size(im,2),length(s));
+for i=1:length(s)
+   for j=1:length(o)
+       se = strel('line',s(i),o(j));
+       imol(:,:,i,j) = imopen(im,se);
+   end
+   se = strel('disk',round(s(i)/2));
+   imod(:,:,i) = imopen(im,se);
+end
+%% Diff
+imd = zeros(size(im,1),size(im,2),length(s));
+imr = zeros(size(im,1),size(im,2),length(s));
+imm = zeros(size(im,1),size(im,2),length(s));
+
+triv = imod==0;
+for i=1:length(s)
+   imm(:,:,i) = max(squeeze(imol(:,:,i,:)),[],3);   % Max for all lines
+   imd(:,:,i) = imm(:,:,i) - imod(:,:,i);           % Diff betwen disk and line
+end
+imr(triv) = 0;
+imda = max(imd,[],3);
+imda= Normalize(imda);
+end
---
+++ BowlerHat2D_TEST.m
@@ -0,0 +1,22 @@
+%% Clear
+clc; clear; close all;
+
+%% path
+addpath('./lib')
+
+%% Load Image 
+im=imread('jellyfish.png');
+
+%% Normalize
+% im = rgb2gray(im);                   % use when the data is rgb format
+im = imcomplement(im);               % use when the background is white
+                                       % and vessels are black
+im = Normalize(im);
+%% Granulometry
+si = 15;                              % size of the disk
+no = 30;                              % number of orientation
+bh = BowlerHat2D(im,si,no);           % call the bowler-hat script
+%% Plot
+figure; 
+subplot(121); imagesc(im); colormap jet; axis off; axis equal; axis tight; title('original');
+subplot(122); imagesc(bh); colormap jet; axis off; axis equal; axis tight; title('bowler-hat');
