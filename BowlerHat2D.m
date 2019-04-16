165577b854afb69240e1c6d90639740af3367a2d
Cigdem Sazak <cigdem.sazak@durham.ac.uk>
2019-04-16 20:06:30 +0300

    Update BowlerHat2D.m

--- BowlerHat2D.m
+++ BowlerHat2D.m
@@ -3,23 +3,23 @@ function imda = BowlerHat2D(im,si,no)
 
 o = 0:180/no:180-180/no; %number of orientation
 
-imol = zeros(size(im,1),size(im,2),length(s),length(o));
-imod = zeros(size(im,1),size(im,2),length(s));
-for i=1:length(s)
+imol = zeros(size(im,1),size(im,2),length(si),length(o));
+imod = zeros(size(im,1),size(im,2),length(si));
+for i=1:length(si)
    for j=1:length(o)
-       se = strel('line',s(i),o(j));
+       se = strel('line',si(i),o(j));
        imol(:,:,i,j) = imopen(im,se);
    end
-   se = strel('disk',round(s(i)/2));
+   se = strel('disk',round(si(i)/2));
    imod(:,:,i) = imopen(im,se);
 end
 
-imd = zeros(size(im,1),size(im,2),length(s));
-imr = zeros(size(im,1),size(im,2),length(s));
-imm = zeros(size(im,1),size(im,2),length(s));
+imd = zeros(size(im,1),size(im,2),length(si));
+imr = zeros(size(im,1),size(im,2),length(si));
+imm = zeros(size(im,1),size(im,2),length(si));
 
 triv = imod==0;
-for i=1:length(s)
+for i=1:length(si)
    imm(:,:,i) = max(squeeze(imol(:,:,i,:)),[],3);   % Max for all lines
    imd(:,:,i) = imm(:,:,i) - imod(:,:,i);           % Diff betwen disk and line
 end
